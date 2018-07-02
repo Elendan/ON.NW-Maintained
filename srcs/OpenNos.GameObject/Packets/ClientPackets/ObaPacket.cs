30d748b110e4ee66d0713c4ef9fba9f921d73419
Elendan <arthur.despeaux@hotmail.fr>
2018-07-02 14:38:48 +0200

    add falcon skill

---
+++ srcs/OpenNos.GameObject/Packets/ClientPackets/ObaPacket.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenNos.Core.Serializing;
+
+namespace OpenNos.GameObject.Packets.ClientPackets
+{
+    [PacketHeader("ob_a")]
+    public class ObaPacket : PacketDefinition
+    {
+    }
+}
